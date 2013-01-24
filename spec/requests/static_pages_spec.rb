a91e059468df1b13b487ba16bf6b4f3d30ff04fc
Travis Luong <travis@travisluong.com>
2013-01-24 02:01:12 -0800

    finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Sample App'" do
+
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                                    :text => "SampleApp | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                                    :text => "SampleApp | Help")
+    end
+  end
+
+  describe "contact page" do
+
+    it "should have the h1 'About Us'" do
+
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                                    :text => "SampleApp | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                    :text => "SampleApp | Contact")
+    end
+  end
+end
