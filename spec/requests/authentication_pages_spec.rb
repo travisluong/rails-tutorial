d356ade528a4b096c5c23f74bbf2dfd84d853e3a
Travis Luong <travis@travisluong.com>
2013-02-03 12:07:54 -0800

    add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -93,8 +93,18 @@ describe "AuthenticationPages" do
           specify { response.should redirect_to(signin_path) }
         end
 
-        describe "visiting user index" do
+        describe "visiting the user index" do
           before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
         end
       end
@@ -110,6 +120,18 @@ describe "AuthenticationPages" do
       describe "submitting to the destroy action" do
         before { delete micropost_path(FactoryGirl.create(:micropost)) }
         specify { response.should redirect_to(signin_url) }
+      end
+    end
+
+    describe "in the Relationships controller" do
+      describe "submitting to the create action" do
+        before { post relationships_path }
+        specify { response.should redirect_to(signin_path) }
+      end
+
+      describe "submitting to the destroy action" do
+        before { delete relationship_path(1) }
+        specify { response.should redirect_to(signin_path) }
       end
     end
 
